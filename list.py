bcc4cd42e2059a48d238ab3d2528c02336eb7ae4
annexmwash <machochohannah5@gmail.com>
2025-05-01 12:10:35 +0300

    my list

---
+++ list.py
@@ -0,0 +1,27 @@
+# creating an empty list
+my_list = []
+  # appending elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+   # inserting 15 in the second position
+my_list.insert(1, 15)
+print(my_list)
+   # extending the list with another list
+my_list.extend([50, 60, 70])
+print(my_list)
+   # removing the last element
+my_list.pop()
+print(my_list)
+   # sorting the list in ascending order
+my_list.sort()
+print(my_list)
+   # finding the index value of 30
+index_of_30 = my_list.index(30)
+print(index_of_30)
+print("The index of 30 is:", index_of_30)
+  
+
+
